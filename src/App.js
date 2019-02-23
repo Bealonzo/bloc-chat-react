0e0b5bb5fd53305c39568d0e46a9cd05f56a2eb4
Bealonzo <alonzo.briane@gmail.com>
2019-02-23 11:02:05 -0500

    assingment-64:Complete

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
+import * as firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
+import RoomList from './components/RoomList';
+
+
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDLq-nbqZu7cIae5GNbpr1F453Py_nAe-s",
+    authDomain: "bloc-chat-c6204.firebaseapp.com",
+    databaseURL: "https://bloc-chat-c6204.firebaseio.com",
+    projectId: "bloc-chat-c6204",
+    storageBucket: "bloc-chat-c6204.appspot.com",
+    messagingSenderId: "467838968284"
+};
+firebase.initializeApp(config);
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return ( <
+            div >
+            <
+            RoomList firebase = {
+                firebase
+            }
+            /> <
+            /div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/RoomList.js
@@ -0,0 +1,32 @@
+import React, {
+    Component
+} from 'react';
+
+class RoomList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            Rooms: []
+        };
+        this.roomsRef = this.props.firebase.database().ref('Rooms');
+    }
+    componentDidMount() {
+        this.roomsRef.on('child_added', snapshot => {
+            const room = snapshot.val();
+            room.key = snapshot.key;
+            this.setState({
+                Rooms: this.state.Rooms.concat(room)
+            });
+        });
+    }
+    render() {
+            const roomList = this.state.Rooms.map((room) =>
+						< li key = {room.key} > {room.name} < /li>);
+								
+                return
+								( < ul > {roomList } < /ul>
+									);
+                }
+            }
+            export default RoomList;
